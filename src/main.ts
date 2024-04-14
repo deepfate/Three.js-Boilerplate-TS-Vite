c249043be40233e284a8d4b589b0d45b1985724e
PeanSass <seanspacer123@gmail.com>
2024-04-14 17:18:16 -0500

    Created branch for this tutorial and swapped boilerplate.

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,19 @@ import Stats from 'three/addons/libs/stats.module.js'
 import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
-scene.add(new THREE.GridHelper())
+scene.add(new THREE.AxesHelper(5))
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 1.5
+camera.position.set(1, 2, 3)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-
-  renderer.render(scene, camera) // Render whenever the screen size changes
-})
-
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.addEventListener('change', ()=>{
-  renderer.render(scene, camera) // Render whenever OrbitControls changes
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
 new OrbitControls(camera, renderer.domElement)
@@ -35,54 +28,39 @@ const material = new THREE.MeshNormalMaterial({ wireframe: true })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-renderer.render(scene, camera)
-
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-// GUI
 const gui = new GUI()
 
-// GUI - Camera Controls
-const cameraFolder = gui.addFolder('Camera')
+const cubeFolder = gui.addFolder('Cube')
+cubeFolder.add(cube, 'visible')
+cubeFolder.open()
 
-cameraFolder.add(camera.position, 'x', -10, 10)
-cameraFolder.add(camera.position, 'y', -10, 10)
-cameraFolder.add(camera.position, 'z', -10, 10)
+const positionFolder = cubeFolder.addFolder('Position')
+positionFolder.add(cube.position, 'x', -5, 5)
+positionFolder.add(cube.position, 'y', -5, 5)
+positionFolder.add(cube.position, 'z', -5, 5)
+positionFolder.open()
 
-cameraFolder.add(camera, 'fov', 0, 180, 0.01).onChange( () => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'aspect', 0.00001, 10).onChange( () => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'near', 0.01, 10).onChange( () => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'far', 0.01, 10).onChange( () => {
-  camera.updateProjectionMatrix()
-})
+const rotationFolder = cubeFolder.addFolder('Rotation')
+rotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+rotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+rotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+rotationFolder.open()
 
-cameraFolder.open()
-//Make the code frame rate independant by considering clock delta time
-
-const clock = new THREE.Clock()
-let delta
+const scaleFolder = cubeFolder.addFolder('Scale')
+scaleFolder.add(cube.scale, 'x', -5, 5)
+scaleFolder.add(cube.scale, 'y', -5, 5)
+scaleFolder.add(cube.scale, 'z', -5, 5)
+scaleFolder.open()
 
 function animate() {
-  requestAnimationFrame(animate)
+    requestAnimationFrame(animate)
 
-  //cube.rotation.x += 0.01
-  //cube.rotation.y += 0.01
+    renderer.render(scene, camera)
 
-  delta = clock.getDelta()
-  cube.rotation.x += delta
-  cube.rotation.y += delta
-
-
-  renderer.render(scene, camera)
-
-  stats.update()
+    stats.update()
 }
 
 animate()
